621fe89fa0e44af9a6e87f134f111c8d67f9ea91
csmith495 <tekF@ce1994>
2022-04-26 18:33:00 -0500

    Complete Cookie Clicker codelab code

---
+++ app/src/main/java/com/example/cookieclicker/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.cookieclicker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    private var currentScore = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val imgCookie : ImageView = findViewById(R.id.imgCookie)
+        val lblTotal : TextView = findViewById(R.id.lblTotal)
+
+        imgCookie.setOnClickListener {
+            currentScore++
+            lblTotal.text = "$currentScore"
+        }
+    }
+}
